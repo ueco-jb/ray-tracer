4a713c174022f6db911c62e3ea7afbd59c350fae
ueco <ueco@libertymail.net>
2020-06-21 12:59:25 +0200

    Added error handling in Canvas type

--- canvas/src/lib.rs
+++ canvas/src/lib.rs
@@ -9,6 +9,11 @@ pub struct Canvas {
     canvas: std::vec::Vec<std::vec::Vec<color::Color>>,
 }
 
+#[derive(Debug)]
+pub enum CanvasError {
+    OutOfCanvasBorder,
+}
+
 impl Canvas {
     pub fn get_width(&self) -> usize {
         self.width
@@ -18,12 +23,21 @@ impl Canvas {
         self.height
     }
 
-    pub fn pixel_at(&self, w: usize, h: usize) -> color::Color {
-        self.canvas[w][h]
+    pub fn pixel_at(&self, w: usize, h: usize) -> Result<color::Color, CanvasError> {
+        if w > self.width - 1 || h > self.height - 1 {
+            Err(CanvasError::OutOfCanvasBorder)
+        } else {
+            Ok(self.canvas[w][h])
+        }
     }
 
-    pub fn write_pixel(&mut self, w: usize, h: usize, c: color::Color) {
-        self.canvas[w][h] = c;
+    pub fn write_pixel(&mut self, w: usize, h: usize, c: color::Color) -> Result<(), CanvasError> {
+        if w > self.width - 1 || h > self.height - 1 {
+            Err(CanvasError::OutOfCanvasBorder)
+        } else {
+            self.canvas[w][h] = c;
+            Ok(())
+        }
     }
 }
 
@@ -37,11 +51,11 @@ pub fn canvas(w: usize, h: usize) -> Canvas {
 }
 
 pub fn pixel_at(canvas: &Canvas, w: usize, h: usize) -> color::Color {
-    canvas.pixel_at(w, h)
+    canvas.pixel_at(w, h).unwrap()
 }
 
 pub fn write_pixel(canvas: &mut Canvas, w: usize, h: usize, c: color::Color) {
-    canvas.write_pixel(w, h, c);
+    canvas.write_pixel(w, h, c).unwrap();
 }
 
 #[cfg(test)]
@@ -49,10 +63,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn create_canvas() {
+    fn color_pixel_on_canvas() {
         let mut c = canvas(10, 20);
         let red = color::color(1, 0, 0);
-        c.write_pixel(2, 3, red);
-        assert_eq!(red, c.pixel_at(2, 3));
+        c.write_pixel(2, 3, red).unwrap();
+        assert_eq!(red, c.pixel_at(2, 3).unwrap());
+    }
+
+    #[test]
+    fn write_pixel_out_of_canvas_border() {
+        let mut c = canvas(1, 2);
+        let red = color::color(1, 0, 0);
+        let write_result = c.write_pixel(20, 30, red);
+        let get_result = c.pixel_at(20, 30);
+        assert!(write_result.is_err());
+        assert!(get_result.is_err());
     }
 }
