57455f8fc2dbb21db814b3fc734571e10b1754ff
ueco <ueco@libertymail.net>
2020-06-23 17:34:46 +0200

    Clippy and fmt fixes

--- src/serialize.rs
+++ src/serialize.rs
@@ -1,8 +1,11 @@
+#![allow(dead_code)]
+
 use std::fs::File;
 use std::io::Write;
 
-fn save(data: &String, path: &String) -> std::io::Result<()> {
+fn save(data: &str, path: &str) -> std::io::Result<()> {
     let mut file = File::create(path)?;
-    file.write_all(data.as_bytes()).expect("Unable to write file");
+    file.write_all(data.as_bytes())
+        .expect("Unable to write file");
     Ok(())
 }
