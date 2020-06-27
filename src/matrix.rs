de4f9fdc6c8612d2e1d809ba6ee29d6f638dcd10
Jakub Bogucki <ext-lux-jb@ad.datcon.co.uk>
2020-06-27 23:03:34 +0200

    First matrix test

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,4 +1,19 @@
-struct Matrix {
+struct Matrix4 {
     matrix: [[i32;4];4],
 }
+
+pub fn matrix4 () -> Matrix4 {
+    Matrix4: {
+        matrix: [i32;4];4] = [[0i32;4];4];
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn constructing_inspecting_4_matrix():
+        let matrix = matrix();
 
